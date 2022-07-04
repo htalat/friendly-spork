f08ee6cc48b6b64186a3baafe77ab48cc6ba4bde
Hassan Talat <hassan.talat.92@gmail.com>
2022-07-04 15:22:01 +0500

    fix url

--- src/services/authorService.js
+++ src/services/authorService.js
@@ -2,7 +2,7 @@ import http from './httpService';
 import { apiUrl, recipeUrl } from '../config.json';
 
 export function getAuthors() {
-    return http.get(`${recipeUrl}/recipes/authors`).then(response => {
+    return http.get(`${recipeUrl}/authors`).then(response => {
         return response.data;
     });
 }
